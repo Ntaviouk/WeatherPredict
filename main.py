1f0d18d4d0c322ea6b09e22dc4340ed24ec7d37b
Ntaviouk <dimasdaniuk@gmail.com>
2025-02-17 16:01:40 +0200

    changet file structure

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from TemperaturePrediction import generate_model, predict_temperature
+from scraper4 import get_weather_data as gd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    generate_model(gd("prcp"), gd("snow"), gd("wdir"), gd("wspd"), gd("wpgt"), gd("pres"),
+                   gd("tsun"), gd("tavg"))
+
+    print(predict_temperature(0, 0, 0, 0, 0, 0, 0, ))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- scraper4.py
+++ scraper4.py
@@ -1,18 +1,34 @@
 from meteostat import Daily, Point
 from datetime import datetime
 
-# Встановлення координат міста (Київ)
-kyiv = Point(50.45, 30.52)
 
-# Встановлення періоду (від - до)
-start = datetime(2024, 1, 1)
-end = datetime(2024, 12, 1)
+def get_weather_data(param):
+    kyiv = Point(50.45, 30.52)
+    start = datetime(2020, 1, 1)
+    end = datetime(2024, 12, 1)
+    data = Daily(kyiv, start, end).fetch()
 
-# Отримання погодних даних
-data = Daily(kyiv, start, end).fetch()
+    if param in data.columns:  # Перевіряємо, чи є параметр у даних
+        return data[param].fillna(0).tolist()  # Замінюємо NaN на 0 і конвертуємо в список
+    else:
+        raise ValueError(f"Параметр '{param}' не знайдено у даних!")
 
-# Вивід потрібних параметрів
-#print(data[['tavg', 'tmin', 'tmax', 'pres', 'wspd']])
 
-tavg_list = data['prcp'].tolist()
-print(tavg_list)
+
+# # Встановлення координат міста (Київ)
+# kyiv = Point(50.45, 30.52)
+#
+# # Встановлення періоду (від - до)
+# start = datetime(2024, 1, 1)
+# end = datetime(2024, 12, 1)
+#
+# # Отримання погодних даних
+# data = Daily(kyiv, start, end).fetch()
+#
+# # Вивід потрібних параметрів
+# # print(data[['tavg', 'tmin', 'tmax', 'pres', 'wspd']])
+#
+# print(data.columns)
+#
+# tavg_list = data['wspd'].tolist()
+# print(tavg_list)
