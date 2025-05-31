ed2ca1d5410dee9fb182dc9e157857a36a10a6eb
Ntaviouk <dimasdaniuk@gmail.com>
2025-05-31 13:57:21 +0300

    added requirements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     generate_model(gd("prcp"), gd("snow"), gd("wdir"), gd("wspd"), gd("wpgt"), gd("pres"),
                    gd("tsun"), gd("tavg"))
 
-    print(predict_temperature(  0.1 ,  110.0,  247.0,   6.7,  31.5,  1013.9,   0.0))
+    print(predict_temperature(0.1, 110.0, 247.0, 6.7, 31.5, 1013.9, 0.0))
 
 
 if __name__ == '__main__':
